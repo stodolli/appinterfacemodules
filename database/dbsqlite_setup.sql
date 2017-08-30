87c998bf6d5f83ab49b939b6e81e3d1a4eb05474
Stefjord Todolli <todolli@biomaps.rutgers.edu>
2017-08-30 14:39:36 -0400

    Add database setup script

---
+++ database/dbsqlite_setup.sql
@@ -0,0 +1,27 @@
+CREATE TABLE simulations (
+  simulation_id INTEGER UNIQUE PRIMARY KEY,
+  nrl INTEGER(3),
+  dnasteps_num INTEGER,
+  proteins_num INTEGER(3),
+
+);
+
+CREATE TABLE structures (
+  simulationid
+);
+
+CREATE TABLE dnasteps (
+
+);
+
+CREATE TABLE proteintypes (
+
+);
+
+CREATE TABLE proteins (
+
+);
+
+CREATE TABLE histonetails (
+
+);
